a3439ec8c7b157dda98458ecfc51044414e5f25d
dedysoerya <dedysoerya@gmail.com>
2022-07-12 06:59:48 +0700

    tes koneksi dan blink tes nodemcu

--- nodemcu-dtb/src/main.cpp
+++ nodemcu-dtb/src/main.cpp
@@ -2,8 +2,15 @@
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(BUILTIN_LED, OUTPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(BUILTIN_LED,HIGH);
+  delay(100);
+  digitalWrite(BUILTIN_LED,LOW);
+  delay(100);
+
 }
